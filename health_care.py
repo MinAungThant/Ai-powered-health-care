1adf1b7627427ba48f7bcc944243c9b3e2867d6f
MinAungThant <minaungthant8@gmail.com>
2024-06-10 21:35:55 +0630

    Update health_care.py

--- health_care.py
+++ health_care.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-
+from sklearn.model_selection import train_test_split, RandomizedSearchCV, cross_val_score
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report
